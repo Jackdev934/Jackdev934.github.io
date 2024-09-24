8fdb70c122814a4bd5f8b2cb2f82ef86425b2137
JackDev934 <143029131+JackDev394@users.noreply.github.com>
2024-09-23 22:14:40 -0400

    updated

--- csce242/exercises/07_js_nav/script.js
+++ csce242/exercises/07_js_nav/script.js
@@ -24,4 +24,43 @@ document.getElementById("move-down").onclick = () => {
     document.getElementsById("ball").style.setProperty("top",pos +"px");
 }
 
+/*show color message */
+document.getElementById("btn-show-color").onclick = () =>{
+    const color = doument.getElementById("txt-enter-color").value.toLowerCase().trim();
+    const messageP = document.getElementById("color-message");
+    let mood = "";
 
+    if(color == "red"){
+        mood = "angry";
+    }
+    else if (color == "blue"){
+        mood = "moody";
+    }
+    else{
+        mood = "undefined";
+    }
+
+    messageP.innerHTML =`You chose ${color} so you are ${mood}`; /* Use back ticks for these */
+
+    }
+
+    /*Donations*/
+    /*get the number 
+    -if it is not a number or it is a negative, 
+    show an error in the error otherwise first show the percent out 
+    of 10000 towards goal Next show a visual */
+    document.getElementById("btn-donate").onclick =() =>{
+        const donation = document.getElementById("txt-donations");
+        const error = document.getElementById("error-donation");
+        error.innerHTML = "";
+        
+        if((donation) || donation < 0){
+            error.innerHTML = "Invalid"
+            return;
+        }
+
+        const percentGoal = donation/goal * 100;
+        console.log(percentGoal + "%" );
+        document.querySelector(":root").style.setProperty("--funds", percentGoal + "%" );
+
+    }
