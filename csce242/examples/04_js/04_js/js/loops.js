4d02f1ef28e30a49e44302c481306ee115a6a69d
Jackdev934 <Jackdev934@gmail.com>
2025-09-23 12:35:30 -0400

    boss

---
+++ csce242/examples/04_js/04_js/js/loops.js
@@ -0,0 +1,12 @@
+document.getElementById("btn-display").onclick = () => {
+    const displaySection = document.getElementById("loop-result");
+    const ul = document.createElement("ul");
+    displaySection.append(ul);
+
+    //loop and write the numbers 1-10
+    for(let i = 0; i < 10; i++ ){
+        const li = document.createElement("li");
+        li.innerHTML= `${i} banana(s)`;
+        ul.append(li);
+    }
+}
