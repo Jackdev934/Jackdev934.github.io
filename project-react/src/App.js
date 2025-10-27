aa6876973427f101ffd9684f9298492a36de91fa
= <jackdev934@gmail.com>
2025-10-27 18:55:44 -0400

    static added

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
 import Bosses from "./pages/Bosses";
 import Characters from "./pages/Characters";
